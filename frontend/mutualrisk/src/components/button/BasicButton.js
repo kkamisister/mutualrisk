b7821ea66da5ab6abe82e7b04c2074993ceaef72
김하연 <hayeonful@gmail.com>
2024-09-12 16:25:38 +0900

    feat: Basic Button 컴포넌트 생성
    Jira:S11P21A607-303

--- frontend/mutualrisk/src/components/button/BasicButton.js
+++ frontend/mutualrisk/src/components/button/BasicButton.js
@@ -1,13 +1,26 @@
 import * as React from 'react';
-// import Stack from '@mui/material';
 import { Button } from '@mui/material';
+import { styled } from '@mui/system';
+import { colors } from 'constants/colors';
 
-export default function BasicButton({ children }) {
+const CustomButton = styled(Button)(() => ({
+	width: '90px',
+	height: '30px',
+	borderRadius: '60px',
+	backgroundColor: colors.main.primary100,
+	border: `1px solid ${colors.main.primary400}`,
+	color: colors.text.sub2,
+	cursor: 'pointer',
+	'&:hover': {
+		backgroundColor: colors.main.primary400,
+		color: colors.background.white,
+	},
+}));
+
+export default function BasicButton({ children, sx, ...props }) {
 	return (
-		// <Stack direction="row">
-		// 	<Button variant="contained" sx={{ borderRadius: 3 }}>
-		// 		{children}
-		// 	</Button>
-		// </Stack>
+		<CustomButton sx={{ ...sx }} {...props}>
+			{children}
+		</CustomButton>
 	);
 }
