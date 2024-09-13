2762bc3bb94e20dc8d90c8e5f24efc82a0b32990
YoonjiKim <jiwabin01@ajou.ac.kr>
2024-09-13 14:57:22 +0900

    feat: MainLayout 헤더 삽입
    MainLayout 헤더 생성
    로그인 버튼 임시 생성
    
    Jira: S11P21A607-334

--- frontend/mutualrisk/src/layouts/MainLayout.js
+++ frontend/mutualrisk/src/layouts/MainLayout.js
@@ -1,7 +1,32 @@
 import React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar'
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import { colors } from 'constants/colors';
 
 const MainLayout = () => {
-	return <div></div>;
+	return (
+    <AppBar 
+			position="static"
+			sx={{
+				backgroundColor: colors.background.primary,
+			}}
+			>
+			<Toolbar>
+				<Box sx={{ flexGrow: 1}}>
+				</Box>
+				<Button 
+					sx={{
+						color: colors.text.main,
+						fontSize: '12px',
+					}}>
+					<Typography>로그인</Typography>
+				</Button>
+			</Toolbar>
+    </AppBar>
+	);
 };
 
 export default MainLayout;
---
+++ frontend/mutualrisk/src/pages/home/HomePage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import MainLayout from 'layouts/MainLayout';
+
+const HomePage = () => {
+	return (
+    <MainLayout>
+    </MainLayout>
+  );
+};
+
+export default HomePage;
