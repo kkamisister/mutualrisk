a17ffcb6423f0d447ff116b852a818c0b4639deb
YoonjiKim <jiwabin01@ajou.ac.kr>
2024-09-19 13:01:40 +0900

    fix: prettier 적용
    일부 파일 prettier 적용하여 코드 정렬
    
    Jira: S11P11A607-334

--- frontend/mutualrisk/src/layouts/MainLayout.js
+++ frontend/mutualrisk/src/layouts/MainLayout.js
@@ -6,88 +6,86 @@ import { colors } from 'constants/colors';
 import logo from 'assets/images/logo.png';
 
 const MainLayout = () => {
-  const location = useLocation();
-  const navigate = useNavigate();
-  const isHomePage = location.pathname === '/'; // HomePage 여부 확인
+	const location = useLocation();
+	const navigate = useNavigate();
+	const isHomePage = location.pathname === '/'; // HomePage 여부 확인
 
-  const headerRef = useRef(null); // 헤더 요소를 참조하기 위한 ref
-  const [headerHeight, setHeaderHeight] = useState(0); // 헤더 높이 저장
+	const headerRef = useRef(null); // 헤더 요소를 참조하기 위한 ref
+	const [headerHeight, setHeaderHeight] = useState(0); // 헤더 높이 저장
 
-  // 헤더 높이를 계산하여 상태에 설정
-  useEffect(() => {
-    if (headerRef.current) {
-      setHeaderHeight(headerRef.current.offsetHeight);
-    }
-  }, []);
+	// 헤더 높이를 계산하여 상태에 설정
+	useEffect(() => {
+		if (headerRef.current) {
+			setHeaderHeight(headerRef.current.offsetHeight);
+		}
+	}, []);
 
-  return (
-    <>
-      <Stack
-        direction="row"
-        ref={headerRef} // ref를 Stack에 연결
-        sx={{
-          position: 'fixed',
-          top: 0,
-          left: 0,
-          width: '100%',
-          backgroundColor: colors.background.primary,
-          borderBottom: 'solid 1px',
-          padding: '10px 80px',
-          alignItems: 'center',
-          justifyContent: 'space-between',
-          zIndex: 1000,
-          boxSizing: 'border-box',
-        }}
-      >
-        {/* 로고 */}
-        <Box
-          component="img"
-          src={logo}
-          alt="logo"
-          sx={{ 
-						height: '30px', 
-						width: 'auto', 
+	return (
+		<>
+			<Stack
+				direction="row"
+				ref={headerRef} // ref를 Stack에 연결
+				sx={{
+					position: 'fixed',
+					top: 0,
+					left: 0,
+					width: '100%',
+					backgroundColor: colors.background.primary,
+					borderBottom: 'solid 1px',
+					padding: '10px 80px',
+					alignItems: 'center',
+					justifyContent: 'space-between',
+					zIndex: 1000,
+					boxSizing: 'border-box',
+				}}>
+				{/* 로고 */}
+				<Box
+					component="img"
+					src={logo}
+					alt="logo"
+					sx={{
+						height: '30px',
+						width: 'auto',
 						cursor: 'pointer',
 						padding: '8px',
 					}}
-          onClick={() => navigate('/')}
-        />
-        {/* '로그인' 버튼 : HomePage에서만 노출 */}
-        {isHomePage && (
-          <Box
-            sx={{
+					onClick={() => navigate('/')}
+				/>
+				{/* '로그인' 버튼 : HomePage에서만 노출 */}
+				{isHomePage && (
+					<Box
+						sx={{
 							width: '60px',
 							textAlign: 'center',
-              cursor: 'pointer',
-              padding: '7px 15px',
-              borderRadius: '5px',
-              fontWeight: 'bold',
-              color: colors.text.sub1,
-              transition: 'background-color 0.3s ease, color 0.3s ease',
-              whiteSpace: 'nowrap',
-              '&:hover': {
-                backgroundColor: colors.background.box,
-                color: colors.main.primary400,
-              },
-            }}
-            onClick={() => navigate('/login')}
-          >
-            로그인
-          </Box>
-        )}
-      </Stack>
+							cursor: 'pointer',
+							padding: '7px 15px',
+							borderRadius: '5px',
+							fontWeight: 'bold',
+							color: colors.text.sub1,
+							transition: 'background-color 0.3s ease, color 0.3s ease',
+							whiteSpace: 'nowrap',
+							'&:hover': {
+								backgroundColor: colors.background.box,
+								color: colors.main.primary400,
+							},
+						}}
+						onClick={() => navigate('/login')}>
+						로그인
+					</Box>
+				)}
+			</Stack>
 
-      <Box 
+			<Box
 				sx={{
-          width: '100%',
-          height: '100%',
+					width: '100%',
+					height: '100%',
 					padding: `${headerHeight + 20}px 20px 20px 20px`,
-          backgroundColor: colors.background.primary,
+					backgroundColor: colors.background.primary,
 				}}>
 				<Outlet />
-      </Box>
-    </>
-  );
+			</Box>
+		</>
+	);
 };
 
 export default MainLayout;
