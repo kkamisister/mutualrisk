09469612b6b81078d7a10a8296b15d11607033fb
YoonjiKim <jiwabin01@ajou.ac.kr>
2024-09-20 16:59:52 +0900

    feat: 포트폴리오 현황 요약 컴포넌트 생성
    PortfolioDetailPage, PortfolioSummaryList 생성
    
    Jira: S11P21A607-373

--- frontend/mutualrisk/src/pages/portfolio/detail/PortfolioDetailPage.js
+++ frontend/mutualrisk/src/pages/portfolio/detail/PortfolioDetailPage.js
@@ -1,14 +1,26 @@
 import React from 'react';
-import FundManagerList from 'pages/fund/list/FundManagerList';
 import BoxTitle from 'components/title/BoxTitle';
-import { Box, Divider, Stack } from '@mui/material';
-import Grid from '@mui/material/Grid';
+import { Avatar, Box, Stack } from '@mui/material';
 import { colors } from 'constants/colors';
+import StockAddBox from 'pages/portfolio/detail/StockAddBox';
+import PortfolioSummaryList from 'pages/portfolio/detail/PortfolioSummaryList';
+
 const PortfolioDetailPage = () => {
 	return (
 		<Stack spacing={2} sx={{ backgroundColor: colors.background.primary }}>
+			<StockAddBox />
 			<Stack spacing={1}>
 				<BoxTitle title="포트폴리오 현황 요약" />
+				<PortfolioSummaryList />
+			</Stack>
+			<Stack spacing={1}>
+				<BoxTitle title="보유 자산 비율" />
+			</Stack>
+			<Stack spacing={1}>
+				<BoxTitle title="그래프1" />
+			</Stack>
+			<Stack spacing={1}>
+				<BoxTitle title="그래프2" />
 			</Stack>
 		</Stack>
 	);
