dff706a2da52fd3ec64c2a78ef78331bb1b87200
김경현 <sujipark2009@gmail.com>
2024-09-20 22:22:53 +0900

    feat: 사용하지 않는 import 제거
    Jira: S11P21A607-350

--- backend/spring/src/main/java/com/example/mutualrisk/common/config/QuerydslConfig.java
+++ backend/spring/src/main/java/com/example/mutualrisk/common/config/QuerydslConfig.java
@@ -3,6 +3,8 @@ package com.example.mutualrisk.common.config;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import lombok.RequiredArgsConstructor;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
