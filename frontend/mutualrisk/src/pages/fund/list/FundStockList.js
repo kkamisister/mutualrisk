d7587c2a2f582da5ba35f216dcbc2d05d5484990
DESKTOP-MMCQPV1\SSAFY <kyp0416@gmail.com>
2024-09-20 09:58:08 +0900

    refactor: 펀드사 종목 리스트 제목 공통 컴포넌트로 변경
    펀드사 종목 리스트 제목 공통 컴포넌트(Title)로 변경
    
    Jira: S11P11A607-364

--- frontend/mutualrisk/src/pages/fund/list/FundStockList.js
+++ frontend/mutualrisk/src/pages/fund/list/FundStockList.js
@@ -1,8 +1,10 @@
 import React from 'react';
-import { Box, Divider, Stack } from '@mui/material';
+import { Stack } from '@mui/material';
 import FundStockListItem from './FundStockListItem';
 import { colors } from 'constants/colors';
-import FundStockBarChart from './FundStockBarChart';
+import FundStockBarChart from 'pages/fund/list/FundStockBarChart';
+import Title from 'components/title/Title';
+
 const FundStockList = ({ title, data }) => {
 	return (
 		<Stack
@@ -13,14 +15,7 @@ const FundStockList = ({ title, data }) => {
 				borderRadius: '20px',
 				border: `solid 1px ${colors.point.stroke}`,
 			}}>
-			<Box
-				sx={{
-					fontSize: '20px',
-					fontWeight: 'bold',
-					color: colors.text.main,
-				}}>
-				{'보유량 TOP 30 종목'}
-			</Box>
+			<Title text={title} caption="(최근 업데이트: 2024/12/31)"></Title>
 			<FundStockBarChart />
 			<FundStockListItem />
 			<FundStockListItem />
