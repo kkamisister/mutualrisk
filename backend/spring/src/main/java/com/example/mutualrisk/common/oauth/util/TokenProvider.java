28d8c8953c7dd7723408fc04cfc3238d264fb673
김경현 <sujipark2009@gmail.com>
2024-09-22 15:33:40 +0900

    fix: parseClaims 수정
    에러를 여기가 아닌 더 상위에서 잡도록 설정
    
    Jira: S11P21A607-350

--- backend/spring/src/main/java/com/example/mutualrisk/common/oauth/util/TokenProvider.java
+++ backend/spring/src/main/java/com/example/mutualrisk/common/oauth/util/TokenProvider.java
@@ -10,6 +10,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 
+import com.example.mutualrisk.common.exception.ErrorCode;
+import com.example.mutualrisk.common.exception.MutualRiskException;
 import com.example.mutualrisk.common.oauth.dto.AuthToken;
 import com.example.mutualrisk.common.oauth.service.TokenService;
 
@@ -31,6 +33,7 @@ public class TokenProvider {
 
 	// 1시간
 	private static final long ACCESS_TOKEN_VALIDITY_SECONDS = 1000 * 60 * 60L;
+	// private static final long ACCESS_TOKEN_VALIDITY_SECONDS = 1000 * 60;
 	// 일주일
 	private static final long REFRESH_TOKEN_VALIDITY_SECONDS = 1000 * 60 * 60 * 24L * 7;
 
@@ -78,15 +81,21 @@ public class TokenProvider {
 	}
 
 	private Claims parseClaims(String accessToken){
-		try{
-			return Jwts.parser().verifyWith(secretKey).build()
-				.parseSignedClaims(accessToken)
-				.getPayload();
 
-		}catch(ExpiredJwtException e){
-			log.warn("만료된 토큰입니다");
-			return e.getClaims();
-		}
+		return Jwts.parser().verifyWith(secretKey).build()
+			.parseSignedClaims(accessToken)
+			.getPayload();
+		// try{
+		// 	return Jwts.parser().verifyWith(secretKey).build()
+		// 		.parseSignedClaims(accessToken)
+		// 		.getPayload();
+		//
+		// }catch(ExpiredJwtException e){
+		// 	log.warn("만료된 토큰입니다");
+		// 	log.warn("e.getMessage() : {}",e.getMessage());
+		// 	log.warn(String.valueOf(e.getClaims()));
+		// 	return e.getClaims();
+		// }
 	}
 	public AuthToken generate(Integer userId){
 
@@ -110,13 +119,13 @@ public class TokenProvider {
 			// 해커가 토큰을 탈취하고 재발급을 받았다면, 내가 가진 토큰과는 다르므로 찾지 못할것임
 			// 그때 예외가 발생
 			String refreshToken = tokenService.findByAccessToken(accessToken)
-				.orElseThrow(() -> new RuntimeException("토큰 재발급에 문제가 발생하였습니다."));
+				.orElseThrow(() -> new MutualRiskException(ErrorCode.TOKEN_REISSUE_FAIL));
 
 			log.warn("발견한 리프레시 토큰 : {}",refreshToken);
 
 			// refreshToken이 아직 유효하다면, accessToken을 재발급 받는다
 			if(validateToken(refreshToken)){
-				String subject = extractSubject(accessToken);
+				String subject = extractSubject(refreshToken);
 				AuthToken authToken = generate(Integer.parseInt(subject));
 				log.warn("재발급 된 엑세스 토큰 : {}",authToken.accessToken());
 				return authToken.accessToken();
