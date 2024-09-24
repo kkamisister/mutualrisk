a1db0df99c66df65a76416dcb272cfcd5dd49a24
김경현 <sujipark2009@gmail.com>
2024-09-24 17:47:33 +0900

    fix: fetchOne() 에 limit 추가
    limit(1) 추가
    
    Jira: S11P21A607-350

--- backend/spring/src/main/java/com/example/mutualrisk/common/repository/ExchangeRatesRepositoryCustomImpl.java
+++ backend/spring/src/main/java/com/example/mutualrisk/common/repository/ExchangeRatesRepositoryCustomImpl.java
@@ -2,6 +2,9 @@ package com.example.mutualrisk.common.repository;
 
 import static com.example.mutualrisk.common.entity.QExchangeRates.*;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class ExchangeRatesRepositoryCustomImpl extends Querydsl4RepositorySupport implements ExchangeRatesRepositoryCustom {
 
     // 가장 최신의 환율 정보를 불러온다
@@ -11,6 +14,7 @@ public class ExchangeRatesRepositoryCustomImpl extends Querydsl4RepositorySuppor
             .from(exchangeRates)
             .where(exchangeRates.curUnit.eq("USD"))
             .orderBy(exchangeRates.exRateDate.desc())
+            .limit(1)
             .fetchOne();
     }
 }
