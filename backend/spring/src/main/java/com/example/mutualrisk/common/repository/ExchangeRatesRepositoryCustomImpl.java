1f297fd3062addd4c673ded1e31cc8539a1f59d7
조용수 <soo_0201@naver.com>
2024-09-24 15:50:50 +0900

    feat: 환율 정보 구하는 로직 추가
    Jira: S11P21A607-453

--- backend/spring/src/main/java/com/example/mutualrisk/common/repository/ExchangeRatesRepositoryCustomImpl.java
+++ backend/spring/src/main/java/com/example/mutualrisk/common/repository/ExchangeRatesRepositoryCustomImpl.java
@@ -1,10 +1,14 @@
 package com.example.mutualrisk.common.repository;
 
-//import static com.example.mutualrisk.asset.entity.QExchangeRates.*;
+import static com.example.mutualrisk.common.entity.QExchangeRates.*;
 
 public class ExchangeRatesRepositoryCustomImpl extends Querydsl4RepositorySupport implements ExchangeRatesRepositoryCustom {
     @Override
     public Double getRecentExchangeRate() {
-        return 0.0;
+        return select(exchangeRates.dbr)
+            .from(exchangeRates)
+            .where(exchangeRates.curUnit.eq("USD"))
+            .orderBy(exchangeRates.exRateDate.desc())
+            .fetchOne();
     }
 }
