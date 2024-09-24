3e95176de028a625b40d98692eb6316ea7882863
김경현 <sujipark2009@gmail.com>
2024-09-24 17:17:04 +0900

    feat: CORS 설정
    CORS 설정 추가
    
    Jira: S11P21A607-350

---
+++ backend/spring/src/main/java/com/example/mutualrisk/common/config/WebMvcConfig.java
@@ -0,0 +1,22 @@
+package com.example.mutualrisk.common.config;
+
+import java.util.List;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**")
+			.allowCredentials(true)
+			.allowedOrigins(
+				"http://localhost:3000","https://j11a607.p.ssafy.io"
+			)
+			.allowedMethods("GET","POST","PUT","DELETE","OPTIONS")
+			.allowedHeaders("*")
+			.exposedHeaders("*");
+	}
+}
