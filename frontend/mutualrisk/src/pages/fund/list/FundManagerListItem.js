f7dd1d2ee81334ed2647b20a1e2b6323318f9687
김하연 <hayeonful@gmail.com>
2024-09-25 19:25:51 +0900

    fix: 수평스크롤 list 및 내부 item 수정
    fund manager list와 내부 item, stock list와 내부 item 공통 component로 수정
    Jira: S11P21A607-459, S11P21A607-458, S11P21A607-456

--- frontend/mutualrisk/src/pages/fund/list/FundManagerListItem.js
+++ frontend/mutualrisk/src/pages/fund/list/FundManagerListItem.js
@@ -1,56 +1,18 @@
 import React from 'react';
-import Avatar from '@mui/material/Avatar';
-import Box from '@mui/material/Box';
-import Stack from '@mui/material/Stack';
-import { colors } from 'constants/colors';
+import ScrollItem from 'components/scroll/ScrollItem';
 import { useNavigate } from 'react-router-dom';
 
 const FundManagerListItem = ({ id, name, capital, imagePath, clicked }) => {
 	const navigate = useNavigate();
+
 	return (
-		<Stack
-			sx={{
-				width: '100px',
-				height: '140px',
-				borderRadius: '20px',
-				marginRight: '5px',
-				cursor: 'pointer',
-				fontWeight: clicked ? 'bold' : 'bold',
-				color: clicked ? colors.text.main : colors.text.sub1, // 폰트 및 이미지 컬러
-				backgroundColor: clicked
-					? colors.background.box
-					: colors.background.white, // 배경
-
-				transition:
-					'background-color 0.3s ease, color 0.3s ease, width 0.3s ease', // 부드러운 애니메이션
-
-				'&:hover': {
-					backgroundColor: colors.background.box, // hover시 일괄적으로 회색 유지
-				},
-				userSelect: 'none',
-				border: `solid 1px ${colors.point.stroke}`,
-			}}
-			onClick={() => {
-				navigate(`/fund/detail/${id}`);
-			}}>
-			<Stack
-				sx={{
-					justifyContent: 'center',
-					alignItems: 'center',
-					marginTop: '10px',
-				}}
-				spacing={0.75}>
-				<Avatar
-					style={{ width: '64px', height: '64px' }}
-					alt="펀드사 이미지"
-					src={imagePath}
-				/>
-				<Box sx={{ fontSize: '14px' }}>{name}</Box>
-				<Box sx={{ fontSize: '12px' }}>
-					${capital.toLocaleString('ko-KR')}
-				</Box>
-			</Stack>
-		</Stack>
+		<ScrollItem
+			name={name}
+			imagePath={imagePath}
+			capital={capital}
+			clicked={clicked}
+			onClick={() => navigate(`/fund/detail/${id}`)}
+		/>
 	);
 };
 
