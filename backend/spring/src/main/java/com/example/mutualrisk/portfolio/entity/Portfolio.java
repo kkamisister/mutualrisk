e01bb0a113544801426337b9a3aaaef06d503b30
조용수 <soo_0201@naver.com>
2024-09-26 16:45:46 +0900

    feat: Portfolio Document 추가
    MongoDB용 document로 변경
    
    Jira: S11P21A607-428

--- backend/spring/src/main/java/com/example/mutualrisk/portfolio/entity/Portfolio.java
+++ backend/spring/src/main/java/com/example/mutualrisk/portfolio/entity/Portfolio.java
@@ -1,50 +1,22 @@
 package com.example.mutualrisk.portfolio.entity;
 
-import java.util.ArrayList;
-import java.util.Base64;
 import java.util.List;
-
-import com.example.mutualrisk.asset.entity.RecommendAsset;
-import com.example.mutualrisk.common.entity.BaseEntity;
-import com.example.mutualrisk.user.entity.User;
-
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+import org.springframework.data.mongodb.core.mapping.FieldType;
 
-@Entity
+@Document(collection = "portfolio")
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor(access = AccessLevel.PROTECTED)
-public class Portfolio extends BaseEntity {
+@Builder
+public class Portfolio {
 
-	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Integer id;
-
-	@ManyToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "user_id")
-	private User user;
-
-	@OneToMany(mappedBy = "portfolio")
-	private List<RecommendAsset> recommendAssetList;
-
-	/**
-	 * 포트폴리오를 추가한 사용자를 지정한다
-	 *
-	 * @param user
-	 */
-	public void setUser(User user){
-		this.user = user;
-		user.getPortfolioList().add(this);
-	}
-
+	@Id @Field(value = "_id",targetType = FieldType.OBJECT_ID)
+	private String id;
+	private String type;
+	private Integer userId;
+	private List<PortfolioAsset> asset;
 }
