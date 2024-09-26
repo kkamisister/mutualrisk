a34f34d8486c0a74a692cc2870311b456f7bb16b
김하연 <hayeonful@gmail.com>
2024-09-26 10:31:15 +0900

    feat: 제약 조건 설정 컴포넌트 생성
    Jira: S11P21A607-386

---
+++ frontend/mutualrisk/src/pages/portfolio/create/condition/AssetConstraintList.js
@@ -0,0 +1,6 @@
+import { Box } from '@mui/material';
+const AssetConstraintList = () => {
+	return <Box>제약조건 설정이용</Box>;
+};
+
+export default AssetConstraintList;
--- frontend/mutualrisk/src/pages/portfolio/create/condition/ConditionSetting.js
+++ frontend/mutualrisk/src/pages/portfolio/create/condition/ConditionSetting.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import { Box } from '@mui/material';
+import Title from 'components/title/Title';
 
-const ConditionSetting = () => {
+const ConditionSetting = ({ assets }) => {
 	return (
 		<Box>
-			<Box>제약조건</Box>
+			<Title
+				text="제약 조건"
+				caption="필요에 따라 종목별 최솟값, 최댓값을 설정할 수 있습니다."
+			/>
 		</Box>
 	);
 };
