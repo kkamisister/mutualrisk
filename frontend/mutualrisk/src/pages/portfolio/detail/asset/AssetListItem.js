baed8341ba19c05c3951b4f521174e6cf2f11f64
YoonjiKim <jiwabin01@ajou.ac.kr>
2024-10-01 12:49:03 +0900

    design: AssetListItem css 수정
    StockItemCard 컴포넌트 사용
    자산 비율 정보 Box height 조정

--- frontend/mutualrisk/src/pages/portfolio/detail/asset/AssetListItem.js
+++ frontend/mutualrisk/src/pages/portfolio/detail/asset/AssetListItem.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { Box, Typography, Avatar, Stack } from '@mui/material';
+import { Box, Typography, Stack } from '@mui/material';
 import { colors } from 'constants/colors';
+import StockItemCard from 'components/card/StockItemCard';
 
 const AssetListItem = ({ asset }) => {
 	return (
@@ -10,49 +11,15 @@ const AssetListItem = ({ asset }) => {
 				width: '500px',
 				maxWidth: '100%',
 				display: 'flex',
-				justifyContent: 'space-evenly',
-				alignItems: 'center',
+				justifyContent: 'space-between',
+				alignItems: 'stretch',
 				marginBottom: '9px',
 			}}>
-			<Box
-				sx={{
-					display: 'flex',
-					justifyContent: 'space-between',
-					alignItems: 'center',
-					backgroundColor: colors.background.box,
-					borderRadius: '20px',
-					padding: '10px',
-					marginRight: '8px',
-					flex: 1, // 부모 높이를 차지하게 설정
-					height: '50px',
-					minHeight: '0', // 부모 높이에 따라 높이 제한
-				}}>
-				{/* 좌측 아이콘 및 정보 */}
-				<Box
-					sx={{
-						display: 'flex',
-					}}>
-					<Avatar
-						src={asset.imagePath}
-						alt={asset.name}
-						sx={{ width: '40px', height: '40px', marginRight: '15px' }}
-					/>
-					<Box
-						sx={{
-							display: 'flex',
-							flexDirection: 'column',
-							justifyContent: 'space-evenly',
-						}}>
-						<Typography sx={{ fontSize: '16px', fontWeight: 'bold' }}>
-							{asset.name}
-						</Typography>
-						<Typography
-							sx={{ fontSize: '13px', color: colors.text.sub2 }}>
-							{asset.ticker}({asset.market})
-						</Typography>
-					</Box>
-				</Box>
-				{/* 중간 금액 정보 */}
+			<StockItemCard
+				code={asset.ticker}
+				name={asset.name}
+				market={asset.market}
+				image={asset.imagePath}>
 				<Box
 					sx={{
 						height: '100%',
@@ -60,7 +27,6 @@ const AssetListItem = ({ asset }) => {
 						flexDirection: 'column',
 						justifyContent: 'space-evenly',
 						textAlign: 'right',
-						// marginRight: '20px',
 					}}>
 					<Typography
 						sx={{ color: 'red', fontWeight: 'bold', fontSize: '16px' }}>
@@ -70,21 +36,24 @@ const AssetListItem = ({ asset }) => {
 						{asset.currentPrice}원
 					</Typography>
 				</Box>
-			</Box>
+			</StockItemCard>
+
+			{/* 자산 비율 정보 */}
 			<Box
 				sx={{
 					backgroundColor: colors.main.primary400,
-					borderRadius: '20px',
-					color: '#FFFFFF',
 					padding: '10px',
+					borderRadius: '10px',
+					color: '#FFFFFF',
 					minWidth: '60px',
 					textAlign: 'center',
 					fontSize: '20px',
 					fontWeight: 'bold',
-					height: '50px',
-					minHeight: '0',
-					maxWidth: '30%',
-					alignContent: 'center',
+					marginLeft: '9px',
+					display: 'flex',
+					justifyContent: 'center',
+					alignItems: 'center',
+					flex: 1,
 				}}>
 				{asset.ratio}%
 			</Box>
