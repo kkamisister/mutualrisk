21db715c0eff74f18167a3312a0a780902a9bf7a
김하연 <hayeonful@gmail.com>
2024-10-02 15:57:15 +0900

    fix: useAssetStore 수정
    파일명 오타 수정
    assets(종목)/total cash(자산 규모) 수정
    
    Jira: S11P21A607-511

--- frontend/mutualrisk/src/stores/useAssetStore.js
+++ frontend/mutualrisk/src/stores/useAssetStore.js
@@ -2,19 +2,33 @@ import { create } from 'zustand';
 
 const useAssetStore = create(set => ({
 	assets: [], // 자산 정보를 저장할 배열
+	totalCash: 0, // 총 자산을 저장할 변수
+
 	addAsset: asset =>
 		set(state => ({
-			assets: [...state.assets, asset], // 자산 추가
+			assets: [...state.assets, asset],
 		})),
+
 	removeAsset: assetId =>
 		set(state => ({
-			assets: state.assets.filter(asset => asset.id !== assetId), // 자산 제거
+			assets: state.assets.filter(asset => asset.id !== assetId),
 		})),
+
 	updateAsset: updatedAsset =>
 		set(state => ({
 			assets: state.assets.map(asset =>
 				asset.id === updatedAsset.id ? updatedAsset : asset
-			), // 자산 정보 업데이트
+			),
+		})),
+
+	addTotalCash: cash =>
+		set(() => ({
+			totalCash: cash,
+		})),
+
+	updateTotalCash: cash =>
+		set(() => ({
+			totalCash: cash,
 		})),
 }));
 
