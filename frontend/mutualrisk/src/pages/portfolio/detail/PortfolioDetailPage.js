92a6d84b8dec77401b15c7ee7212547c886cacbe
YoonjiKim <jiwabin01@ajou.ac.kr>
2024-10-04 00:00:10 +0900

    feat: 전체 포트폴리오 조회 api 연결
    select box 위치 수정,
    fetchPortfolioList 연결
    
    Jira: S11P11A607-506

--- frontend/mutualrisk/src/pages/portfolio/detail/PortfolioDetailPage.js
+++ frontend/mutualrisk/src/pages/portfolio/detail/PortfolioDetailPage.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { useQuery } from '@tanstack/react-query';
+import { useNavigate } from 'react-router-dom';
 import TitleDivider from 'components/title/TitleDivider';
 import Title from 'components/title/Title';
 import Select from 'react-select';
@@ -12,14 +14,75 @@ import BackTesting from 'pages/portfolio/detail/graph/BackTesting';
 import EquitySectors from 'pages/portfolio/detail/graph/EquitySectors';
 import MonthlyReturns from 'pages/portfolio/detail/graph/MonthlyReturns';
 import WidgetContainer from 'components/container/WidgetConatiner';
+import { fetchPortfolioList } from 'utils/apis/analyze';
+
+// 날짜 포맷
+const formatDate = dateString => {
+	const date = new Date(dateString);
+	return date.toLocaleDateString('ko-KR', {
+		year: 'numeric',
+		month: '2-digit',
+		day: '2-digit',
+	});
+};
 
 const PortfolioDetailPage = () => {
+	const navigate = useNavigate();
+
+	const { data, isLoading, isError } = useQuery({
+		queryKey: ['portfolioList'],
+		queryFn: fetchPortfolioList,
+	});
+
+	// 로딩중
+	if (isLoading) {
+		return <div>Loading...</div>;
+	}
+
+	// 에러 처리
+	if (isError) {
+		return <div>Error fetching portfolio list.</div>;
+	}
+
+	const { hasPortfolio, portfolioList } = data;
+
+	// 포트폴리오가 없을 경우 리다이렉트 어데로???
+	if (!hasPortfolio) {
+		navigate('/');
+		return null;
+	}
+
+	const portfolioOptions = portfolioList.map(portfolio => ({
+		value: portfolio.id,
+		label: portfolio.name,
+		createdAt: portfolio.createdAt,
+	}));
+
+	// Select 커스텀
+	const CustomOption = ({ data, innerRef, innerProps }) => (
+		<div ref={innerRef} {...innerProps} style={{ padding: '10px' }}>
+			<span>{data.label}</span>
+			<br />
+			<span style={{ color: '#999', fontSize: '12px' }}>
+				생성일: {formatDate(data.createdAt)}
+			</span>
+		</div>
+	);
+
 	return (
 		<Stack spacing={2} sx={{ backgroundColor: colors.background.primary }}>
-			<TitleDivider text="내 포트폴리오" />
-			<Select />
 			<StockAddBox />
-			{/* 포트폴리오 현황 요약 */}
+			<TitleDivider text="내 포트폴리오" />
+
+			<Select
+				options={portfolioOptions}
+				placeholder="포트폴리오 선택"
+				onChange={selectedOption => {
+					console.log(selectedOption.value); // 선택한 포트폴리오 ID
+				}}
+				components={{ Option: CustomOption }}
+			/>
+
 			<WidgetContainer sx={{ backgroundColor: colors.main.primary100 }}>
 				<Title
 					text="포트폴리오 현황 요약"
