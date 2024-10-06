3de4302c07e4b5429c67ee2007ce7a52a389e20a
YoonjiKim <jiwabin01@ajou.ac.kr>
2024-10-06 20:27:47 +0900

    design: AssetRatioPieChart css 수정
    중앙에 해당 종목 보유 비율 표시
    
    Jira: S11P11A607-520

--- frontend/mutualrisk/src/pages/portfolio/detail/asset/AssetRatioPieChart.js
+++ frontend/mutualrisk/src/pages/portfolio/detail/asset/AssetRatioPieChart.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback, useMemo } from 'react';
 import { PieChart, Pie, ResponsiveContainer, Cell, Sector } from 'recharts';
 import { colors } from 'constants/colors';
 
@@ -6,25 +6,31 @@ const AssetRatioPieChart = ({ assets }) => {
 	const [activeIndex, setActiveIndex] = useState(0);
 	const [textWidth, setTextWidth] = useState(0);
 
-	const onPieEnter = (_, index) => {
+	// Pie hover 시 실행되는 함수
+	const onPieEnter = useCallback((_, index) => {
 		setActiveIndex(index);
-	};
+	}, []);
 
 	// assets 데이터를 PieChart용 데이터로 변환
-	const chartData = assets.map((asset, index) => ({
-		name: asset.name, // 자산 이름
-		value: asset.weight * 100, // 자산 비율 (퍼센트로 변환)
-		color: asset.color || colors.piechart[index % colors.piechart.length], // 색상을 colors.piechart에서 가져오기
-	}));
+	const chartData = useMemo(
+		() =>
+			assets.map((asset, index) => ({
+				name: asset.name, // 자산 이름
+				value: asset.weight * 100, // 자산 비율 (퍼센트로 변환)
+				color:
+					asset.color || colors.piechart[index % colors.piechart.length], // 색상을 colors.piechart에서 가져오기
+			})),
+		[assets]
+	);
 
 	// 텍스트 너비 측정 함수
-	const getTextWidth = (text, font) => {
+	const getTextWidth = useCallback((text, font) => {
 		const canvas = document.createElement('canvas');
 		const context = canvas.getContext('2d');
 		context.font = font;
 		const metrics = context.measureText(text);
 		return metrics.width;
-	};
+	}, []);
 
 	// 활성화된 섹터가 변경될 때마다 텍스트 너비를 업데이트
 	useEffect(() => {
@@ -33,62 +39,77 @@ const AssetRatioPieChart = ({ assets }) => {
 			const width = getTextWidth(chartData[activeIndex].name, font);
 			setTextWidth(width);
 		}
-	}, [activeIndex, chartData]);
+	}, [activeIndex, chartData, getTextWidth]);
 
 	// 활성화된 섹터의 정보를 렌더링하는 함수
-	const renderActiveShape = props => {
-		const {
-			cx,
-			cy,
-			innerRadius,
-			outerRadius,
-			startAngle,
-			endAngle,
-			fill,
-			payload,
-		} = props;
+	const renderActiveShape = useCallback(
+		props => {
+			const {
+				cx,
+				cy,
+				innerRadius,
+				outerRadius,
+				startAngle,
+				endAngle,
+				fill,
+				payload,
+			} = props;
 
-		const hoverInnerRadius = innerRadius - 5;
-		const hoverOuterRadius = outerRadius + 10;
+			const hoverInnerRadius = innerRadius - 5;
+			const hoverOuterRadius = outerRadius + 10;
 
-		return (
-			<g>
-				{/* 활성화된 섹터 */}
-				<Sector
-					cx={cx}
-					cy={cy}
-					innerRadius={hoverInnerRadius}
-					outerRadius={hoverOuterRadius}
-					startAngle={startAngle}
-					endAngle={endAngle}
-					fill={fill}
-				/>
+			return (
+				<g>
+					{/* 활성화된 섹터 */}
+					<Sector
+						cx={cx}
+						cy={cy}
+						innerRadius={hoverInnerRadius}
+						outerRadius={hoverOuterRadius}
+						startAngle={startAngle}
+						endAngle={endAngle}
+						fill={fill}
+					/>
 
-				{/* 텍스트 너비에 맞춰 rect 너비 설정 */}
-				<rect
-					x={cx - textWidth / 2 - 10} // 텍스트의 절반 너비만큼 위치 조정하고, 여유 공간 추가
-					y={cy - 16}
-					width={textWidth + 20} // 텍스트 너비보다 20px 더 크게 설정
-					height={32}
-					fill="white"
-					opacity={0.1}
-					rx={10}
-					ry={10}
-				/>
+					{/* 텍스트와 rect 요소 */}
+					<rect
+						x={cx - textWidth / 2 - 10} // 텍스트의 절반 너비만큼 위치 조정하고, 여유 공간 추가
+						y={cy - 32} // rect 위치를 더 위로 조정하여 두 줄 텍스트 공간 확보
+						width={textWidth + 20} // 텍스트 너비보다 20px 더 크게 설정
+						height={64} // rect의 높이를 증가시켜서 두 줄 텍스트가 들어갈 공간 확보
+						fill="white"
+						opacity={0.8}
+						rx={10}
+						ry={10}
+					/>
 
-				{/* 종목명 텍스트 */}
-				<text
-					x={cx}
-					y={cy}
-					dy={8}
-					textAnchor="middle"
-					fill={fill}
-					fontSize="20px">
-					{payload.name}
-				</text>
-			</g>
-		);
-	};
+					{/* 종목명 텍스트 */}
+					<text
+						x={cx}
+						y={cy - 10} // 첫 번째 텍스트 위치 조정
+						textAnchor="middle"
+						fill={fill}
+						fontSize="22px"
+						// fontWeight="bold"
+					>
+						{payload.name}
+					</text>
+
+					{/* 퍼센티지 텍스트 */}
+					<text
+						x={cx}
+						y={cy + 18} // 두 번째 텍스트 위치를 더 아래로 조정
+						textAnchor="middle"
+						fill="#333"
+						fontSize="25px"
+						fontWeight="bold">
+						{`${payload.value.toFixed(1)}%`} {/* 소수점 2자리까지 표시 */}
+					</text>
+				</g>
+			);
+		},
+		[textWidth]
+	);
 
 	return (
 		<div
@@ -112,8 +133,8 @@ const AssetRatioPieChart = ({ assets }) => {
 						nameKey="name"
 						cx="50%"
 						cy="50%"
-						innerRadius={90}
-						outerRadius={160}
+						innerRadius={120}
+						outerRadius={180}
 						fill="#8884d8"
 						onMouseEnter={onPieEnter}
 						stroke="none">
