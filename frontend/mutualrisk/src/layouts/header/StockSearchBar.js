ce2969500636537e23df2998b3ec9603daaf059f
meowingwhitey <kyp0416@gmail.com>
2024-10-06 18:41:26 +0900

    feat: 엔터 누르면 첫번째 검색 결과로 navigate
    Jira: S11P21A607-523

--- frontend/mutualrisk/src/layouts/header/StockSearchBar.js
+++ frontend/mutualrisk/src/layouts/header/StockSearchBar.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import SearchIcon from '@mui/icons-material/Search';
-import { Autocomplete, Box, TextField } from '@mui/material';
+import { Autocomplete, Avatar, Box, TextField, Stack } from '@mui/material';
 import { colors } from 'constants/colors';
 import { useQuery } from '@tanstack/react-query';
 import { fetchAssetsByKeyword } from 'utils/apis/asset';
@@ -10,11 +10,23 @@ const StockSearchBar = () => {
 	const [keyword, setKeyword] = useState('');
 	const navigate = useNavigate();
 
+	// Use react-query to fetch search results based on the keyword
 	const { isLoading, data: searchResult = [] } = useQuery({
-		queryKey: ['stockSearchResult', keyword], // keyword를 queryKey에 포함하여 키워드가 변경되면 새로운 요청 실행
+		queryKey: ['stockSearchResult', keyword],
 		queryFn: () => fetchAssetsByKeyword(keyword),
-		enabled: !!keyword, // 키워드가 있을 때만 요청 실행
+		enabled: !!keyword,
 	});
+
+	// Function to handle Enter key to navigate to the first result
+	const handleEnterKey = event => {
+		if (event.key === 'Enter' && searchResult.length > 0) {
+			// Set the TextField value to the first result's name
+			setKeyword(searchResult[0].name);
+			// Navigate to the detail page of the first result
+			navigate(`/stock/detail/${searchResult[0].assetId}`);
+		}
+	};
+
 	return (
 		<Box
 			sx={{
@@ -33,15 +45,49 @@ const StockSearchBar = () => {
 			<Autocomplete
 				freeSolo
 				options={searchResult}
-				getOptionLabel={option => {
-					return option.name;
-				}} // label을 사용하도록 정의
+				autoHighlight
+				inputValue={keyword} // This binds the input value to keyword state
+				onInputChange={(event, newValue) => {
+					setKeyword(newValue); // Update keyword when input changes
+				}}
+				getOptionLabel={option => option.name}
+				renderOption={(props, option) => (
+					<Stack
+						{...props}
+						direction="row"
+						spacing={1}
+						sx={{
+							alignItems: 'center',
+							padding: '10px 10px 10px 10px',
+							margin: '0 10px 0 10px',
+							borderRadius: '10px',
+							cursor: 'pointer',
+							'&:hover': {
+								backgroundColor: colors.background.box,
+							},
+						}}
+						onClick={() => {
+							navigate(`/stock/detail/${option.assetId}`);
+						}}>
+						<Avatar
+							sx={{ width: 36, height: 36 }}
+							src={`https://j11a607.p.ssafy.io/stockImage/${option.code}.png`}
+						/>
+						<Box sx={{ fontWeight: 'bold', fontSize: '14px' }}>
+							{option.name}
+						</Box>
+						<Box
+							sx={{
+								color: colors.text.sub2,
+								fontSize: '14px',
+							}}>{`${option.code}(${option.market})`}</Box>
+					</Stack>
+				)}
 				renderInput={params => (
 					<TextField
+						{...params}
 						placeholder="어떤 종목을 찾으세요?"
-						onChange={event => {
-							setKeyword(event.target.value);
-						}}
+						onKeyDown={handleEnterKey} // Handle Enter key
 						sx={{
 							'& .MuiOutlinedInput-root': {
 								height: '40px', // 높이 변경
@@ -53,7 +99,6 @@ const StockSearchBar = () => {
 								},
 							},
 						}}
-						{...params}
 					/>
 				)}
 				fullWidth
