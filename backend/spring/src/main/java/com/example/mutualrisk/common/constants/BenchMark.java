efdd24dcd6e2474495cb92916353f31de6e5eba9
조용수 <soo_0201@naver.com>
2024-10-06 18:14:11 +0900

    feat: BenchMark ETF constant로 추가
    Jira: S11P21A607-524

---
+++ backend/spring/src/main/java/com/example/mutualrisk/common/constants/BenchMark.java
@@ -0,0 +1,11 @@
+package com.example.mutualrisk.common.constants;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class BenchMark {
+    private Integer assetId;
+    private Integer sectorId;
+}
