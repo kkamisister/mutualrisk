2c543144ac6e4dba965869694411b3f8dceb654d
김하연 <hayeonful@gmail.com>
2024-10-07 17:46:46 +0900

    fix: 제약조건 관리 zustand로 변경
    Jira: S11P21A607-391, S11P21A607-542, S11P21A607-563

--- frontend/mutualrisk/src/pages/portfolio/create/condition/AssetConstraintList.js
+++ frontend/mutualrisk/src/pages/portfolio/create/condition/AssetConstraintList.js
@@ -1,27 +1,27 @@
-import { useState, useEffect } from 'react';
-import { Box, Input, Tooltip, Stack, Typography } from '@mui/material';
+import { useState } from 'react';
+import { Box, Input, Tooltip, Stack, Typography, Button } from '@mui/material';
 import BasicButton from 'components/button/BasicButton';
 import BasicChip from 'components/chip/BasicChip';
 import { colors } from 'constants/colors';
 import { fetchPortfolioList } from 'utils/apis/analyze';
-import { createPortfolio } from 'utils/apis/portfolio';
 import useAssetStore from 'stores/useAssetStore';
+import useConstraintStore from 'stores/useConstraintStore';
 import { useQuery } from '@tanstack/react-query';
 import EditIcon from '@mui/icons-material/Edit';
-import { exportAs } from '@mui/x-data-grid/internals';
 
 const AssetConstraintList = ({ assets }) => {
 	const [hasPortfolio, setHasPortfolio] = useState(false);
 	const totalCash = useAssetStore(state => state.totalCash);
-	const [openInputModal, setOpenInputModal] = useState(false);
-	const [lowerBounds, setLowerBounds] = useState(Array(assets.length).fill(0));
-	const [upperBounds, setUpperBounds] = useState(Array(assets.length).fill(1));
-	const [exactProportion, setExactProportion] = useState(
-		Array(assets.length).fill(null)
-	);
-	const [isOverLimit, setIsOverLimit] = useState(
-		Array(assets.length).fill(false)
-	); // 초과 여부 상태 추가
+	const {
+		lowerBounds,
+		upperBounds,
+		exactProportion,
+		isLowerBoundExceeded,
+		isUpperBoundUnderLimit,
+		setLowerBound,
+		setUpperBound,
+		setExactProportion,
+	} = useConstraintStore();
 
 	useQuery({
 		queryKey: ['portfolioList'],
@@ -31,33 +31,7 @@ const AssetConstraintList = ({ assets }) => {
 		},
 	});
 
-	// totalCash가 변경될 때마다 초과 여부를 업데이트
-	useEffect(() => {
-		const updatedOverLimit = assets.map(asset => asset.price > totalCash);
-		setIsOverLimit(updatedOverLimit);
-	}, [totalCash, assets]);
-
-	const handleLowerChange = (index, value) => {
-		const updated = [...lowerBounds];
-		updated[index] = value !== '' ? parseFloat(value) / 100 : 0;
-		setLowerBounds(updated);
-	};
-
-	const handleUpperChange = (index, value) => {
-		const updated = [...upperBounds];
-		updated[index] = value !== '' ? parseFloat(value) / 100 : 1;
-		setUpperBounds(updated);
-	};
-
-	const handleExactChange = (index, value) => {
-		const updated = [...exactProportion];
-		updated[index] = value !== '' ? parseFloat(value) / 100 : null;
-		setExactProportion(updated);
-	};
-
-	const handleSubmit = () => {
-		setOpenInputModal(true);
-	};
+	const handleSubmit = () => {};
 
 	return (
 		<Box sx={{ height: '100%', position: 'relative' }}>
@@ -71,84 +45,120 @@ const AssetConstraintList = ({ assets }) => {
 			<Box
 				sx={{
 					pt: 2,
-					height: 'calc(100% - 200px)',
+					height: 'calc(100% - 120px)',
 					overflowY: 'auto',
 					'&::-webkit-scrollbar': { display: 'none' },
 					msOverflowStyle: 'none',
 					scrollbarWidth: 'none',
 				}}>
 				<Stack spacing={2}>
-					{assets.map((asset, index) => (
-						<Stack direction="row" key={asset.assetId} spacing={2}>
-							<Stack
-								sx={{
-									flex: 1,
-									alignItems: 'center',
-									justifyContent: 'center',
-									bgcolor: colors.background.box,
-									borderRadius: '8px',
-									fontSize: '12px',
-									fontWeight: 'bold',
-								}}>
-								{asset.region === 'KR' ? (
-									asset.name
-								) : (
-									<Tooltip title={asset.name}>
-										<span>{asset.code}</span>
-									</Tooltip>
-								)}
+					{assets.map((asset, index) => {
+						const isPriceOverTotalCash = asset.price > totalCash;
+						const lowerBoundTooltip = isLowerBoundExceeded
+							? '최소 비율 합이 100%를 초과했습니다.'
+							: '';
+						const upperBoundTooltip = isUpperBoundUnderLimit
+							? '최대 비율 합이 100%미만입니다.'
+							: '';
+						const priceTooltip = isPriceOverTotalCash
+							? '종목 가격이 총 자산보다 높습니다'
+							: '';
+
+						return (
+							<Stack direction="row" key={asset.assetId} spacing={2}>
+								<Stack
+									sx={{
+										flex: 1,
+										alignItems: 'center',
+										justifyContent: 'center',
+										bgcolor: colors.background.box,
+										borderRadius: '8px',
+										fontSize: '12px',
+										fontWeight: 'bold',
+									}}>
+									{asset.region === 'KR' ? (
+										asset.name
+									) : (
+										<Tooltip title={asset.name}>
+											<span>{asset.code}</span>
+										</Tooltip>
+									)}
+								</Stack>
+
+								<Tooltip title={lowerBoundTooltip || priceTooltip}>
+									<Input
+										type="number"
+										defaultValue={0}
+										onChange={e =>
+											setLowerBound(index, e.target.value)
+										}
+										sx={{
+											flex: 1,
+											backgroundColor:
+												isLowerBoundExceeded || isPriceOverTotalCash
+													? colors.background.red
+													: 'inherit',
+											borderRadius: '8px',
+										}}
+										inputProps={{
+											style: {
+												textAlign: 'center',
+											},
+										}}
+									/>
+								</Tooltip>
+
+								<Tooltip title={upperBoundTooltip || priceTooltip}>
+									<Input
+										type="number"
+										defaultValue={100}
+										onChange={e =>
+											setUpperBound(index, e.target.value)
+										}
+										sx={{
+											flex: 1,
+											backgroundColor:
+												isUpperBoundUnderLimit ||
+												isPriceOverTotalCash
+													? colors.background.red
+													: 'inherit',
+											borderRadius: '8px',
+										}}
+										inputProps={{
+											style: {
+												textAlign: 'center',
+											},
+										}}
+									/>
+								</Tooltip>
+
+								<Tooltip title={priceTooltip}>
+									<Input
+										type="number"
+										defaultValue=""
+										onChange={e =>
+											setExactProportion(index, e.target.value)
+										}
+										sx={{
+											flex: 1,
+											backgroundColor: isPriceOverTotalCash
+												? colors.background.red
+												: 'inherit',
+											borderRadius: '8px',
+										}}
+										inputProps={{
+											style: {
+												textAlign: 'center',
+											},
+										}}
+									/>
+								</Tooltip>
 							</Stack>
-
-							<Input
-								type="number"
-								defaultValue={0}
-								onChange={e => handleLowerChange(index, e.target.value)}
-								sx={{
-									flex: 1,
-									backgroundColor: isOverLimit[index]
-										? colors.background.red
-										: 'inherit',
-									borderRadius: '8px',
-									textAlign: 'center',
-								}}
-								inputProps={{
-									style: {
-										textAlign: 'center',
-									},
-								}}
-							/>
-
-							<Input
-								borderRadius="8px"
-								type="number"
-								defaultValue={100}
-								onChange={e => handleUpperChange(index, e.target.value)}
-								sx={{
-									flex: 1,
-									backgroundColor:
-										exactProportion < 0 || exactProportion > 100
-											? colors.background.red
-											: 'inherit',
-								}}
-							/>
-
-							<Input
-								fullWidth
-								type="number"
-								defaultValue=""
-								onChange={e => handleExactChange(index, e.target.value)}
-								sx={{
-									flex: 1,
-									backgroundColor:
-										exactProportion < 0 || exactProportion > 100
-											? colors.background.red
-											: 'inherit',
-								}}
-							/>
-						</Stack>
-					))}
+						);
+					})}
 				</Stack>
 			</Box>
+
 			<Stack direction="row" sx={{ alignItems: 'center' }}>
 				<Tooltip
 					title={
@@ -156,12 +166,15 @@ const AssetConstraintList = ({ assets }) => {
 							? '현재 포트폴리오의 자산 가치를 기반으로 산정된 금액입니다.'
 							: ''
 					}>
-					<Typography sx={{ fontWeight: 'bold' }}>
-						총자산 : {totalCash}원
-					</Typography>
+					총자산:{' '}
+					<Input sx={{ fontWeight: 'bold' }} defaultValue={totalCash} /> 원
 				</Tooltip>
-				<EditIcon sx={{ fontSize: '12px', color: colors.text.sub1 }} />
+				<Button>만원</Button>
+				<Button>십만원</Button>
+				<Button>백만원</Button>
+				<Button>천만원</Button>
 			</Stack>
+
 			<Box
 				sx={{
 					position: 'absolute',
