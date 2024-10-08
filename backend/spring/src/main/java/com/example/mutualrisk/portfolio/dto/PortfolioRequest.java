dbf45aef5831ee4460d26c779af55c3724fc56ff
조용수 <soo_0201@naver.com>
2024-10-08 22:48:38 +0900

    feat: 포트폴리오 조회 api response 필드 추가
    리밸런싱 api 정상 동작을 위한 필드 추가(totalCash, 제약 조건)
    
    Jira: S11P21A607-572

--- backend/spring/src/main/java/com/example/mutualrisk/portfolio/dto/PortfolioRequest.java
+++ backend/spring/src/main/java/com/example/mutualrisk/portfolio/dto/PortfolioRequest.java
@@ -13,6 +13,7 @@ import lombok.Builder;
 public record PortfolioRequest() {
 
 
+	@Builder
 	@Schema(name = "포트폴리오 제작 데이터를 담는 DTO",description = "유저가 포트폴리오 제작 시 입력한"
 		+ "정보를 담는다")
 	public record PortfolioInitDto(
@@ -25,7 +26,7 @@ public record PortfolioRequest() {
 		@Schema(name = "유저가 설정한 투자 금액을 입력합니다",
 			example="1,500,000"
 		)
-		Integer totalCash,
+		Double totalCash,
 		@ArraySchema(schema = @Schema(
 			description = "유저가 담은 자산의 id를 담은 리스트여야 합니다.",
 			example = "1,2,3,4,5",
