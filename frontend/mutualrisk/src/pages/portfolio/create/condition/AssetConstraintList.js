d2048bdf4b2a358f8780b098370d5d53c7a461a4
김하연 <hayeonful@gmail.com>
2024-10-08 13:09:58 +0900

    feat: 제약 조건 설정 disabled 추가
    포트폴리오 있는 유저에 대해 제약 조건 disabled 추가
    
    Jira: S11P21A607-549

--- frontend/mutualrisk/src/pages/portfolio/create/condition/AssetConstraintList.js
+++ frontend/mutualrisk/src/pages/portfolio/create/condition/AssetConstraintList.js
@@ -7,7 +7,6 @@ import { fetchPortfolioList } from 'utils/apis/analyze';
 import useAssetStore from 'stores/useAssetStore';
 import useConstraintStore from 'stores/useConstraintStore';
 import { useQuery } from '@tanstack/react-query';
-import EditIcon from '@mui/icons-material/Edit';
 
 const AssetConstraintList = ({ assets }) => {
 	const [hasPortfolio, setHasPortfolio] = useState(false);
@@ -37,7 +36,7 @@ const AssetConstraintList = ({ assets }) => {
 		<Box sx={{ height: '100%', position: 'relative' }}>
 			<Stack direction="row" spacing={2}>
 				<BasicChip label="종목 이름" />
-				<BasicChip label="최솟값 (%)" />
+				<BasicChip label="최솟값(%)" />
 				<BasicChip label="최댓값 (%)" />
 				<BasicChip label="지정 비율 (%)" />
 			</Stack>
@@ -45,7 +44,7 @@ const AssetConstraintList = ({ assets }) => {
 			<Box
 				sx={{
 					pt: 2,
-					height: 'calc(100% - 120px)',
+					height: 'calc(100% - 140px)',
 					overflowY: 'auto',
 					'&::-webkit-scrollbar': { display: 'none' },
 					msOverflowStyle: 'none',
@@ -88,6 +87,7 @@ const AssetConstraintList = ({ assets }) => {
 								<Tooltip title={lowerBoundTooltip || priceTooltip}>
 									<Input
 										type="number"
+										disabled={hasPortfolio}
 										defaultValue={0}
 										onChange={e =>
 											setLowerBound(index, e.target.value)
@@ -99,10 +99,12 @@ const AssetConstraintList = ({ assets }) => {
 													? colors.background.red
 													: 'inherit',
 											borderRadius: '8px',
+											overflow: 'hidden',
 										}}
 										inputProps={{
 											style: {
 												textAlign: 'center',
+												borderRadius: '8px',
 											},
 										}}
 									/>
@@ -111,6 +113,7 @@ const AssetConstraintList = ({ assets }) => {
 								<Tooltip title={upperBoundTooltip || priceTooltip}>
 									<Input
 										type="number"
+										disabled={hasPortfolio}
 										defaultValue={100}
 										onChange={e =>
 											setUpperBound(index, e.target.value)
@@ -123,10 +126,12 @@ const AssetConstraintList = ({ assets }) => {
 													? colors.background.red
 													: 'inherit',
 											borderRadius: '8px',
+											overflow: 'hidden',
 										}}
 										inputProps={{
 											style: {
 												textAlign: 'center',
+												borderRadius: '8px',
 											},
 										}}
 									/>
@@ -135,6 +140,7 @@ const AssetConstraintList = ({ assets }) => {
 								<Tooltip title={priceTooltip}>
 									<Input
 										type="number"
+										disabled={hasPortfolio}
 										defaultValue=""
 										onChange={e =>
 											setExactProportion(index, e.target.value)
@@ -145,10 +151,12 @@ const AssetConstraintList = ({ assets }) => {
 												? colors.background.red
 												: 'inherit',
 											borderRadius: '8px',
+											overflow: 'hidden',
 										}}
 										inputProps={{
 											style: {
 												textAlign: 'center',
+												borderRadius: '8px',
 											},
 										}}
 									/>
@@ -159,31 +167,41 @@ const AssetConstraintList = ({ assets }) => {
 				</Stack>
 			</Box>
 
-			<Stack direction="row" sx={{ alignItems: 'center' }}>
-				<Tooltip
-					title={
-						hasPortfolio
-							? '현재 포트폴리오의 자산 가치를 기반으로 산정된 금액입니다.'
-							: ''
-					}>
-					총자산:{' '}
-					<Input sx={{ fontWeight: 'bold' }} defaultValue={totalCash} /> 원
-				</Tooltip>
-				<Button>만원</Button>
-				<Button>십만원</Button>
-				<Button>백만원</Button>
-				<Button>천만원</Button>
-			</Stack>
-
-			<Box
+			<Stack
+				spacing={2}
 				sx={{
-					position: 'absolute',
-					left: '50%',
-					bottom: 0,
-					transform: 'translateX(-50%)',
+					justifyContent: 'center',
 				}}>
-				<BasicButton text="포트폴리오 제작하기" onClick={handleSubmit} />
-			</Box>
+				<Stack
+					direction="row"
+					justifyContent="center"
+					sx={{ width: '100%' }}>
+					<Tooltip
+						title={
+							hasPortfolio
+								? '현재 포트폴리오의 자산 가치를 기반으로 산정된 금액입니다.'
+								: ''
+						}>
+						<Box display="flex" alignItems="center" flex={2}>
+							총자산:{' '}
+							<Input
+								sx={{ fontWeight: 'bold' }}
+								defaultValue={totalCash}
+							/>{' '}
+							원
+						</Box>
+					</Tooltip>
+					<Button flex={1}>만원</Button>
+					<Button flex={1}>십만원</Button>
+					<Button flex={1}>백만원</Button>
+					<Button flex={1}>천만원</Button>
+				</Stack>
+				<BasicButton
+					text="포트폴리오 제작하기"
+					onClick={handleSubmit}
+					sx={{ alignSelf: 'center' }}
+				/>
+			</Stack>
 		</Box>
 	);
 };
