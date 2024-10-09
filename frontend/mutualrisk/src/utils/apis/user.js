76b62c0176a8e9833809cf6c6fa16b11839c30c7
meowingwhitey <kyp0416@gmail.com>
2024-10-09 16:19:47 +0900

    feat: 로그인 및 로그아웃 호출 함수 분리
    Jira: S11P21A607-545

---
+++ frontend/mutualrisk/src/utils/apis/user.js
@@ -0,0 +1,36 @@
+import axiosInstance from './axiosInstance';
+
+// 로그인 관련 API
+// React-Query 단에서 Mutate가 요구되는 API들(POST, DELETE)은 Promise를 반환하도록
+// 그 외, fetch만 필요한 API들(GET)은 실제 API 응답의 data 맴버 내 실제 데이터를 반환
+
+/**
+ * 카카오 OAuth 코드로 AccessToken 및 사용자명 반환
+ * @param {String} code - code
+ * @returns {Object} -API 문서 참조
+ */
+export const fetchLoginInfoByCode = async code => {
+	const response = await axiosInstance.get(
+		`/oauth/kakao/callback?code=${code}`
+	);
+	const data = response.data;
+	sessionStorage.setItem('accessToken', data.authToken.accessToken);
+	sessionStorage.setItem('name', data.userName);
+
+	// 받은 API Key로 axios instance에 Authorization 설정
+	axiosInstance.defaults.headers.common[
+		'Authorization'
+	] = `Bearer ${data.authToken.accessToken}`;
+	console.log(axiosInstance.defaults.headers.common['Authorization']);
+
+	return response.data;
+};
+
+/**
+ * 로그아웃
+ * @returns {Boolean} -API 문서 참조
+ */
+export const logoutUser = async () => {
+	const response = await axiosInstance.get(`/oauth/kakao/logout`);
+	return response.data.status === 200;
+};
