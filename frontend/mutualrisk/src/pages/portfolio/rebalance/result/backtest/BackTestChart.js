77062909b06d6720ee38f064d986ef8c7f171ead
YoonjiKim <jiwabin01@ajou.ac.kr>
2024-10-10 16:59:07 +0900

    feat: 리밸런싱 진행 시 백테스팅 api 연결
    Jira: S11P11A607-598

--- frontend/mutualrisk/src/pages/portfolio/rebalance/result/backtest/BackTestChart.js
+++ frontend/mutualrisk/src/pages/portfolio/rebalance/result/backtest/BackTestChart.js
@@ -12,79 +12,70 @@ import {
 	ResponsiveContainer,
 } from 'recharts';
 
-const BackTestChart = ({
-	portfolioId,
-	timeInterval = 'month',
-	measure = 'profit',
-	inflationAdjusted = false,
-}) => {
+const BackTestChart = ({ backtestingData }) => {
 	const [data, setData] = useState([]);
+	const [yDomain, setYDomain] = useState([0, 'auto']);
 
 	useEffect(() => {
-		// 백엔드 API 호출 부분을 주석 처리
-		// async function fetchData() {
-		//   try {
-		//     const response = await axios.get(`/api/v1/portfolio/backtest`, {
-		//       params: {
-		//         portfolioId,
-		//         timeInterval,
-		//         measure,
-		//         inflationAdjusted`
-		//       }
-		//     });
-		//     const responseData = response.data.data.performances.map(item => ({
-		//       time: new Date(item.time).toISOString().split('T')[0], // 날짜 형식 변환
-		//       valuation: item.valuation
-		//     }));
-		//     setData(responseData);
-		//   } catch (error) {
-		//     console.error("Error fetching backtesting data:", error);
-		//   }
-		// }
+		if (backtestingData) {
+			const benchmarkPerformances =
+				backtestingData.data.benchMark.performances || [];
+			const portfolioPerformances =
+				backtestingData.data.portfolioValuation.performances || [];
 
-		// 더미 데이터 추가
-		const dummyData = [
-			{ time: '2012-01', portfolioValuation: 10000, benchmark: 10000 },
-			{ time: '2013-01', portfolioValuation: 11000, benchmark: 11500 },
-			{ time: '2014-01', portfolioValuation: 12000, benchmark: 12500 },
-			{ time: '2015-01', portfolioValuation: 13000, benchmark: 13500 },
-			{ time: '2016-01', portfolioValuation: 14000, benchmark: 14000 },
-			{ time: '2017-01', portfolioValuation: 16000, benchmark: 17000 },
-			{ time: '2018-01', portfolioValuation: 18000, benchmark: 19000 },
-			{ time: '2019-01', portfolioValuation: 20000, benchmark: 21000 },
-			{ time: '2020-01', portfolioValuation: 22000, benchmark: 23000 },
-			{ time: '2021-01', portfolioValuation: 25000, benchmark: 26000 },
-			{ time: '2022-01', portfolioValuation: 28000, benchmark: 29000 },
-			{ time: '2023-01', portfolioValuation: 32000, benchmark: 33000 },
-		];
+			const mergedData = benchmarkPerformances
+				.map((benchmarkItem, index) => {
+					const portfolioItem = portfolioPerformances[index] || {};
+					const benchmarkValuation = benchmarkItem.valuation;
+					const portfolioValuation = portfolioItem.valuation;
 
-		setData(dummyData);
-	}, [portfolioId, timeInterval, measure, inflationAdjusted]);
+					if (benchmarkValuation || portfolioValuation) {
+						return {
+							time: new Date(benchmarkItem.time)
+								.toISOString()
+								.split('T')[0],
+							benchmark: benchmarkValuation || 0,
+							portfolioValuation: portfolioValuation || 0,
+						};
+					}
+					return null;
+				})
+				.filter(item => item !== null);
+
+			const allValuations = mergedData.flatMap(item => [
+				item.benchmark,
+				item.portfolioValuation,
+			]);
+			const minValuation = Math.min(...allValuations);
+			const maxValuation = Math.max(...allValuations);
+
+			setYDomain([minValuation, maxValuation]);
+			setData(mergedData);
+		}
+	}, [backtestingData]);
 
 	return (
 		<WidgetContainer>
-			<Title text={'백테스팅'} />
+			<Title text={'백테스팅 결과'} />
 			<ResponsiveContainer width="100%" height={400}>
 				<LineChart
 					data={data}
 					margin={{ top: 20, right: 30, left: 20, bottom: 5 }}>
 					<CartesianGrid strokeDasharray="3 3" />
 					<XAxis dataKey="time" />
-					<YAxis />
+					<YAxis domain={yDomain} />
 					<Tooltip />
 					<Legend />
-					{/* 두 개의 포트폴리오 성과 */}
 					<Line
 						type="monotone"
 						dataKey="portfolioValuation"
 						name="리밸런싱 포트폴리오 평가가치"
 						stroke="#8884d8"
 					/>
-					{/* 예시: SP500과 비교하는 경우 */}
 					<Line
 						type="monotone"
 						dataKey="benchmark"
-						name="SP500 Benchmark"
+						name="기존 포트폴리오 평가가치"
 						stroke="#82ca9d"
 					/>
 				</LineChart>
